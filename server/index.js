674769f6ca7852a3720427e92929b0795c205aff
Johan Magnusson <joma2304@student.miun.se>
2025-03-28 14:51:56 +0100

    Lägg till user typing ...

--- server/index.js
+++ server/index.js
@@ -13,22 +13,40 @@ const app = express()
 app.use(express.static(path.join(__dirname, "public")))
 
 const expressServer = app.listen(PORT, () => {
- console.log(`listening in port ${PORT}`)
- })
+    console.log(`listening in port ${PORT}`)
+})
 
 const io = new Server(expressServer, {
     cors: {
         origin: "process.env.NODE_ENV" === "prodction" ? false :
-        ["http://localhost:5500", "http://127.0.0.1:5500"]
+            ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
 })
 
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
 
+    //Upon connection - only to user
+    socket.emit('message', "Welcome to chat app!")
+
+    //Upon connection - to all others
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`)
+
+    //Listening for message event
     socket.on('message', data => {
         console.log(data)
-        io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    })
+
+    //When user disconnet - to all others
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`)
+    })
+
+    //Listen for activity 
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity', name)
     })
 })
 
