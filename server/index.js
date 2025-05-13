2573f855d52e0371eb8cd5489092f171c6b7db50
Jonas Ståleker <jonasstaleker@gmail.com>
2025-05-13 13:01:48 +0200

    update path for Render

--- server/index.js
+++ server/index.js
@@ -21,10 +21,10 @@ const PORT = process.env.PORT || 3500
 
 
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.use(
